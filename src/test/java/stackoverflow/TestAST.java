6e8224b2f10e26c7c40ff4d421b2371c95ea19c1
saka1029 <saka1029@gmail.com>
2022-07-17 16:51:22 +0900

    Update TestAST.java

--- src/test/java/stackoverflow/TestAST.java
+++ src/test/java/stackoverflow/TestAST.java
@@ -1,62 +1,56 @@
 package stackoverflow;
 
-import static org.junit.Assert.*;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
-import java.util.Stack;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import org.junit.Test;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestAST {
+	
+	/*
+	 * syntax
+	 * body   = name [ '(' [ body { ',' body } ] ')' ]
+	 */
+	static final Pattern PAT = Pattern.compile("[^(),]+|.");
 
-	static final List<String> definedOrder = List.of(
-		"Initial","Final","Deleted","Keep","Exception");
-	static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-    record Rec(int key0, Date key1, List<String> row) {}
-
-	static Date parseDate(String input) {
-		try {
-			return simpleDateFormat.parse(input);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
+	static void parse(String s) {
+		new Object() {
+            Matcher m = PAT.matcher(s);
+            String token = get();
+            String get() { return token = m.find() ? m.group() : null; }
+            boolean isName() { return token != null && "(),".indexOf(token) < 0; }
+            boolean eat(String s) {
+            	if (!Objects.equals(token, s))
+            		return false;
+            	get();
+            	return true;
+            }
+            void body() {
+            	int start = m.start();
+            	String name = token;
+//            	System.out.println("name: " + token);
+            	get(); //skip name
+            	if (eat("(")) {
+                    if (isName()) {
+                        body();
+                        while (eat(","))
+                            body();
+                    }
+                    if (!eat(")"))
+                        throw new RuntimeException(") expected");
+            	}
+                int end = token == null ? s.length() : m.start();
+                System.out.println("body: " + s.substring(start, end));
+            }
+            void parse() {
+            	if (!isName())
+                    throw new RuntimeException("name expected");
+                body();
+            }
+		}.parse();
 	}
-
+	
 	public static void main(String[] args) {
-		List<List<String>> outputToStore = Arrays.asList(
-            Arrays.asList("Final", "331", "M", "22/03/2020 00:00:00"), 
-            Arrays.asList("Initial", "335", "M", "22/06/2022 00:00:00"), 
-            Arrays.asList("Exception", "335", "M", "22/05/2022 00:00:00"), 
-            Arrays.asList("Final", "335", "M", "20/06/2022 00:00:00"), 
-            Arrays.asList("Keep", "335", "M", "02/06/2022 11:00:00"), 
-            Arrays.asList("Final", "335", "M", "10/04/2022 02:00:00"), 
-            Arrays.asList("Deleted", "335", "M", "22/06/2022 15:55:10"),
-            Arrays.asList("Exception", "335", "M", "22/06/2022 15:55:09"), 
-            Arrays.asList("Final", "335", "M", "22/06/2022 15:56:00"), 
-            Arrays.asList("Initial", "335", "M", "11/06/2022 00:00:00")
-        );
-
-		List<List<String>> sorted = outputToStore.stream()
-			.map(e -> new Rec(definedOrder.indexOf(e.get(0)), parseDate(e.get(3)), e))
-			.sorted(Comparator.comparingInt(Rec::key0).thenComparing(Rec::key1))
-			.map(Rec::row)
-			.toList();
-
-		for (List<String> row : sorted)
-			System.out.println(row);
+		String s = "max(sum(1, max(1,2,sum(2,5)), 3, 5, mult(3,3)))";
+		parse(s);
 	}
 }
