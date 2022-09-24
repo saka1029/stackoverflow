dbb5421fbc0012a195d14dde488d15e306903443
saka1029 <saka1029@gmail.com>
2022-09-24 17:47:24 +0900

    aa

--- src/test/java/stackoverflow/TestConcat.java
+++ src/test/java/stackoverflow/TestConcat.java
@@ -3,6 +3,8 @@ package stackoverflow;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,29 +13,36 @@ import org.junit.Test;
 
 public class TestConcat {
 
-    @Test
-    public void test1() {
-        String[] column1Array = "あああ いいい ううう".split(" ");
-        String[] column2Array = "えええ おおお".split(" ");
-        List<String> list = Stream.concat(
-                Arrays.stream(column1Array),
-                Arrays.stream(column2Array))
-            .collect(Collectors.toList());
-        System.out.println(list);
+    public static List<String> sortOrders(List<String> orderList) {
+        return orderList.stream()
+            .map(str -> new Object() {
+                String org = str;
+                String key;
+                {
+                    key = str.replaceFirst("^.* ", "");
+                    if (key.matches(".*\\d.*")) key = "\uffff";
+                }})
+            .sorted(Comparator.comparing(obj -> obj.key))
+            .map(obj -> obj.org)
+            .toList();
+    }
+
+    public static List<String> sortOrders0(List<String> orderList) {
+        // Write your code here
+       Collections.sort( orderList,
+       (a, b) -> a.split(" *", 2)[1].compareTo( b.split(" *", 2)[1] )
+       );
+       
+       return orderList;
     }
 
     @Test
-    public void test2() {
-        String[] column1Array = "あああ いいい ううう".split(" ");
-        String[] column2Array = "えええ おおお".split(" ");
-        String[] column3Array = "かかか ききき".split(" ");
-        List<String> list = Stream.of(
-                column1Array,
-                column2Array,
-                column3Array)
-            .flatMap(array -> Arrays.stream(array))
-            .collect(Collectors.toList());
-        System.out.println(list);
-    }
+    public void test1() {
+    List<String> list = Arrays.asList(
+        "19th apple orange",
+        "17th admin 7th",
+        "19th apple table");
+    System.out.println(sortOrders(list));
+}
 
 }
