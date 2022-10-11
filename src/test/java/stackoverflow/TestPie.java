bbb5822b9e158807831d2ba1280138592bea0bf1
saka1029 <saka1029@gmail.com>
2022-10-11 23:14:20 +0900

    Update TestPie.java

--- src/test/java/stackoverflow/TestPie.java
+++ src/test/java/stackoverflow/TestPie.java
@@ -3,38 +3,36 @@ package stackoverflow;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 public class TestPie {
 
-	static List<List<Integer>> sum(List<Integer> list, int target) {
-		int size = list.size();
+	static List<List<Integer>> sum(List<Integer> in, int target) {
+		int size = in.size();
 		List<List<Integer>> result = new ArrayList<>();
 		int[] count = new int[size];
 		new Object() {
 			
-			void found() {
+			void check(int sum) {
+			    if (sum != target) return;
 				List<Integer> t = new ArrayList<>();
-				for (int i = 0; i < size; ++i) {
-					int e = list.get(i);
-					for (int j = 0, max = count[i]; j < max; ++j)
+				for (int i = 0; i < size; ++i)
+					for (int j = 0, e = in.get(i), max = count[i]; j < max; ++j)
 						t.add(e);
-				}
 				result.add(t);
 			}
 
 			void find(int index, int sum) {
-				if (index >= size) {
-					if (sum == target) found();
-				} else {
-					int e = list.get(index);
-					for (int i = 0, newSum = sum; true; ++i, newSum += e) {
+				if (index >= size)
+				    check(sum);
+				else
+					for (int i = 0, e = in.get(index), newSum = sum; true; ++i, newSum += e) {
 						if (newSum > target) break;
 						count[index] = i;
 						find(index + 1, newSum);
 					}
-				}
 			}
 		}.find(0, 0);
 		return result;
